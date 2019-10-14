1cd71eb67529f43f151195bfa831945588eb16f9
Chris <chriscarton@live.fr>
2019-10-14 12:19:52 +0200

    Updates

---
+++ precache-manifest.544e0e3b0898859c4d99560d273995bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833c26d0f71040768bbd6266a6a3be3a",
+    "url": "/app-original-cosmic/index.html"
+  },
+  {
+    "revision": "e2b21223828c21e3716c",
+    "url": "/app-original-cosmic/static/css/main.1931a4ff.chunk.css"
+  },
+  {
+    "revision": "f60b0a3ddf7d2ea15e8a",
+    "url": "/app-original-cosmic/static/js/2.2806121d.chunk.js"
+  },
+  {
+    "revision": "e2b21223828c21e3716c",
+    "url": "/app-original-cosmic/static/js/main.e55715cf.chunk.js"
+  },
+  {
+    "revision": "710b53499b34b58055a9",
+    "url": "/app-original-cosmic/static/js/runtime-main.565524cb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/app-original-cosmic/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/app-original-cosmic/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/app-original-cosmic/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/app-original-cosmic/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/app-original-cosmic/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "76ad19ce8d0d7b11ac0050edf8fd3aa5",
+    "url": "/app-original-cosmic/static/media/loader.76ad19ce.gif"
+  }
+]);
